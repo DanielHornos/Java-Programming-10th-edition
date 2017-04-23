2879cc1d061da4b58f4689f99cb56abf0bc83eae
DanielHornos <daniel03hv@gmail.com>
2017-04-23 18:20:29 +0200

    Add files via upload

---
+++ src/Chapter_20/Exercise_16.java
@@ -0,0 +1,51 @@
+package Chapter_20;
+
+import ToolKit.PostfixNotation;
+
+/**
+ *
+ * (Convert infix to postfix)
+ * Write a method that converts an infix expression
+ * into a postfix expression using the following header:
+ *
+ * public static String convertToPostfix(String expression)
+ * For example, the method should convert the infix expression
+ * (1 + 2) * 3 to 1 2 + 3 *and2 * (1 + 3)to2 1 3 + *
+ *
+ * Created by Daniel Hornos Valiente on 1/21/17.
+ */
+public class Exercise_16 {
+
+    public static void main(String[] args) {
+        System.out.println(infixToPostfix("10*(2+10)*(10/2+5)+(10/(2*(10*2)+2)+8)"));
+    }
+
+
+    public static String infixToPostfix(String expression) {
+
+        StringBuilder sb = new StringBuilder();
+
+        String operators = "()-+*/";
+
+        // Numbers with spaces in-between will be merged,
+        // Example (9 3 + 3) * 2, the string will be read as ( 93 + 3 ) * 2
+        for (char ch : expression.toCharArray()) {
+
+            if (ch == ' ') continue; // ignore white space
+
+            if (Character.isAlphabetic(ch)) // no letters
+                throw new RuntimeException("No characters allowed");
+
+            if (operators.indexOf(ch) >= 0)
+                sb.append(" ").append(ch).append(" ");
+            else if (Character.isDigit(ch) || ch == '.')
+                sb.append(ch);
+            else
+                throw new RuntimeException("Invalid character! " + ch);
+        }
+
+        return PostfixNotation.convertToPostfix(sb.toString().trim());
+    }
+
+
+}
---
+++ src/Chapter_20/Exercise_19.java
@@ -0,0 +1,120 @@
+package Chapter_20;
+
+import ToolKit.PostfixNotation;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+/**
+ * (Game: solution ratio for 24-point game)
+ * When you pick four cards from a deck of 52 cards for the 24-point game introduced
+ * in Programming Exercise 20.13, the four cards may not have a 24-point solution.
+ * What is the number of all possible picks of four cards from 52 cards? Among all
+ * possible picks, how many of them have 24-point solutions? What is the success
+ * ratio—that is, (number of picks with solutions)/ (number of all possible picks
+ * of four cards)? Write a program to find these answers.
+ *
+ * Created by Daniel Hornos Valiente on 1/23/17.
+ */
+public class Exercise_19 {
+
+    public static void main(String[] args) {
+
+        // Possible card combinations
+        long possiblePermutation = factorial(new BigInteger("52"), new BigInteger("4")).longValue();
+        System.out.println("Possible 52 cards permutation: " + possiblePermutation);
+
+        // Possible permutations
+        String[] operators = allOperators();
+        int possibleSolutions = possibleSolutions(operators);
+        System.out.println("Possible solutions: " + possibleSolutions);
+
+        System.out.println("Success ratio = " + possibleSolutions / (double)possiblePermutation);
+
+        System.out.println(24336.0 / 6497400.0);
+
+    }
+
+    public static int possibleSolutions(String[] operators) {
+
+        ArrayList<String> expression = new ArrayList<>();
+        for (int i = 1; i <= 13; i++)
+            for (int k = 1; k <= 13; k++)
+                for (int j = 1; j <= 13; j++)
+                    for (int m = 1; m <= 13; m++)
+                        for (String s : operators)
+                            expression.add(i + " " + k + " " + j + " " + m +" " + s);
+        int count = 0;
+
+        ArrayList<String> solutions = new ArrayList<>(10000);
+
+        for (String s : expression) {
+            if (PostfixNotation.evaluate(s) == 24) {
+                solutions.add(s);
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static String[] allOperators() {
+
+        String[] op = {"+", "/", "*", "-"};
+        String[] operators = new String[64];
+        int index = 0;
+        for (int i = 0; i < 4; i++)
+            for (int k = 0; k < 4; k++)
+                for (int j = 0; j < 4; j++, index++)
+                    operators[index] = " " + op[i] + " " + op[k] + " " + op[j];
+
+        return operators;
+
+    }
+
+    /**
+     * n! / (n - r)!
+     *
+     * @param n Number of things to choose from
+     * @param r Number of things chosen
+     * @return Number of permutations
+     */
+    private static long factorial(int n, int r) {
+        return factorial(n) / factorial(n - r);
+    }
+
+    /**
+     * n!
+     * @param n start value
+     * @return Factorial value of n
+     */
+    private static long factorial(int n) {
+        if (n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+
+    }
+
+    /**
+     * n! / (n - r)!
+     *
+     * @param n Number of things to choose from
+     * @param r Number of things chosen
+     * @return Number of permutations
+     */
+    private static BigInteger factorial(BigInteger n, BigInteger r) {
+        return factorial(n).divide(factorial(n.subtract(r)));
+    }
+
+    /**
+     * n!
+     * @param n start value
+     * @return Factorial value of n
+     */
+    private static BigInteger factorial(BigInteger n) {
+        if (n.compareTo(BigInteger.ONE) == 0)
+            return BigInteger.ONE;
+        else
+            return n.multiply(factorial(n.subtract(BigInteger.ONE)));
+    }
+
+}
